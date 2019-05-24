948ac9742d410279bb2040868192c761ec78425f
tanfengji <tanfengji2009@gmail.com>
2019-05-24 15:07:56 +0800

    Change from tensorflow to sklearn and keras

--- Titanic Machine Learning from Disaster/titanic.py
+++ Titanic Machine Learning from Disaster/titanic.py
@@ -1,268 +1,113 @@
 import numpy as np
-import tensorflow as tf
 import pandas as pd
-import sklearn.model_selection as model_selection
+from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OneHotEncoder
-# PassengerId, Survived, Pclass, Name, Sex, Age, SibSp, Parch, Ticket, Fare, Cabin, Embarked
-# TBD:
-# 1. Add more features
-# 2. Add more layers/neurons
-# 3. Add regularization
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import cross_val_score
+from keras.models import Sequential
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.layers import Dense
+from sklearn.model_selection import StratifiedKFold
 
 
 def start():
-    dev_data, x_test = load_data()
-    # x_train, x_validation, y_train, y_validation = split_train_validation(dev_data)
-    y_dev = dev_data['Survived']
-    x_dev = dev_data.drop(['Survived'], axis=1)
-    scaling_info = extract_scaling_info(dev_data)
-    encoders = generate_one_hot_encoders()
-    x_train = clean_data(x_dev, scaling_info, encoders)
-    y_train = y_dev.values.reshape(1, -1)
+    dev_data, test_data = load_data()
+    numerical_cols = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
+    numerical_transformer = SimpleImputer(strategy='median')
+    categorical_cols = ['Sex', 'Embarked']
+    categorical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='most_frequent')),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))]
+    )
+    name_col = ['Name']
+    name_transformer = Pipeline(steps=[
+        ('clean_name', FunctionTransformer(clean_name, validate=False)),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))]
+    )
+    ticket_col = ['Ticket']
+    ticket_transformer = Pipeline(steps=[
+        ('clean_ticket', FunctionTransformer(clean_ticket, validate=False)),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))]
+    )
+    selected_cols = numerical_cols + categorical_cols + name_col + ticket_col
+    X_train = dev_data[selected_cols].copy()
+    y_train = dev_data.Survived
+    X_test = test_data[selected_cols].copy()
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numerical_transformer, numerical_cols),
+            ('cat', categorical_transformer, categorical_cols),
+            ('name', name_transformer, name_col),
+            ('ticket', ticket_transformer, ticket_col)
+        ])
 
-    # x_validation = clean_data(x_validation, scaling_info, encoders)
-    # y_validation = y_validation.values.reshape(1, -1)
-
-    # parameters = model(x_train, y_train, x_validation, y_validation, learning_rate=0.001, regularization=0.0003)
-    # predictions = predict(x_validation, parameters).transpose()
-    # int_prediction = []
-    # for i in range(predictions.shape[0]):
-    #     if predictions[i] > 0.5:
-    #         int_prediction.append(1)
-    #     else:
-    #         int_prediction.append(0)
-    # check_performance(int_prediction, y_validation[0])
-
-    # int_prediction = []
-    # for i in range(predictions.shape[0]):
-    #     if predictions[i] > 0.5:
-    #         int_prediction.append(1)
-    #     else:
-    #         int_prediction.append(0)
-    #
-
-    #Determine the best learning rate and regularization parameter
-    #parameters = model(x_train, y_train)
-    # learning_rates = [0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1]
-    # # regularizations = [0, 0.00001, 0.00003, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1]
-    # for learning_rate in learning_rates:
-    #     tf.reset_default_graph()
-    #     model(x_train, y_train, x_validation, y_validation, learning_rate=learning_rate, regularization=0)
-
-    parameters = model(x_train, y_train, learning_rate=0.001, regularization=0.0003)
-    csv_output = x_test['PassengerId'].to_frame()
-    x_test = clean_data(x_test, scaling_info, encoders)
-    predictions = predict(x_test, parameters).transpose()
+    model = KerasClassifier(build_fn=nn_model, epochs=300, batch_size=50)
+    clf = Pipeline(steps=[('preprocessor', preprocessor),
+                          ('model', model)
+                     ])
+    clf.fit(X_train, y_train)
+    # kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=1)
+    # results = cross_val_score(clf, X_train, y_train, cv=kfold)
+    # print(results.mean())
+    preds = clf.predict(X_test)
+    csv_output = test_data['PassengerId'].to_frame()
     int_prediction = []
-    for i in range(predictions.shape[0]):
-        if predictions[i] > 0.5:
+    for pred in preds:
+        if pred > 0.5:
             int_prediction.append(1)
         else:
             int_prediction.append(0)
     csv_output['Survived'] = int_prediction
     csv_output.to_csv('result.csv', index=False)
-
-
-def check_performance(predictions, y_validation):
-    false_positive = 0
-    false_negative = 0
-    for i in range(y_validation.shape[0]):
-        if predictions[i] != y_validation[i]:
-            if predictions[i] == 1:
-                false_positive += 1
-            else:
-                false_negative += 1
-    print(false_positive)
-    print(false_negative)
+    submission = pd.DataFrame()
+    submission['PassengerId'] = test_data.index
+    submission['Survived'] = preds
 
 
 def load_data():
     dev_data = pd.read_csv('train.csv')
-    x_test = pd.read_csv('test.csv')
-    return dev_data, x_test
-
-
-def clean_data(dev_data, scaling_info, encoders):
-    cleaned_pclass = encoders['enc_pclass'].transform(dev_data['Pclass'].fillna(4).values.reshape(-1, 1)).toarray()
-    cleaned_name = encoders['enc_title'].transform(clean_name(dev_data['Name']).reshape(-1, 1)).toarray()
-    cleaned_sex = encoders['enc_gender'].transform(dev_data['Sex'].fillna('Other').values.reshape(-1, 1)).toarray()
-    cleaned_age = scaling_to_one(dev_data['Age'], scaling_info['age']).values.reshape(-1, 1)
-    cleaned_sib_sp = scaling_to_one(dev_data['SibSp'], scaling_info['sib_sp']).values.reshape(-1, 1)
-    cleaned_parch = scaling_to_one(dev_data['Parch'], scaling_info['parch']).values.reshape(-1, 1)
-    cleaned_fare = scaling_to_one(dev_data['Fare'], scaling_info['fare']).values.reshape(-1, 1)
-    cleaned_cabin = encoders['enc_cabin'].transform(clean_cabin(dev_data['Cabin'].fillna('Other')).
-                                                    reshape(-1, 1)).toarray()
-    cleaned_embarked = encoders['enc_embarked'].transform(dev_data['Embarked'].fillna('Other').values.
-                                                          reshape(-1, 1)).toarray()
-    return np.concatenate((cleaned_pclass, cleaned_name, cleaned_sex, cleaned_age, cleaned_sib_sp, cleaned_parch,
-                           cleaned_fare, cleaned_cabin, cleaned_embarked), axis=1).transpose()
-
-
-def scaling_to_one(input, scaling_info):
-    return (input.fillna(scaling_info['mean']) - scaling_info['min'])/(scaling_info['max'] - scaling_info['min'])
-
-
-def split_train_validation(dev_data):
-    y_dev = dev_data['Survived']
-    x_dev = dev_data.drop(['Survived'], axis=1)
-    x_train, x_validation, y_train, y_validation = model_selection.train_test_split(x_dev, y_dev, train_size=0.9,
-                                                                                    test_size=0.1, random_state=1)
-    return x_train, x_validation, y_train, y_validation
-
-
-def extract_scaling_info(dev_data):
-    age = {'min': dev_data['Age'].min(), 'mean': dev_data['Age'].mean(), 'max': dev_data['Age'].max()}
-    fare = {'min': dev_data['Fare'].min(), 'mean': dev_data['Fare'].mean(), 'max': dev_data['Fare'].max()}
-    sib_sp = {'min': dev_data['SibSp'].min(), 'mean': dev_data['SibSp'].mean(), 'max': dev_data['SibSp'].max()}
-    parch = {'min': dev_data['Parch'].min(), 'mean': dev_data['Parch'].mean(), 'max': dev_data['Parch'].max()}
-    scaling_info = {'age': age, 'fare': fare, 'sib_sp': sib_sp, 'parch': parch}
-    return scaling_info
-
-
-def generate_one_hot_encoders():
-    # pclass 4 and other in other encoders are for missing entries
-    enc_pclass = OneHotEncoder(handle_unknown='ignore').fit([[1], [2], [3], [4]])
-    enc_gender = OneHotEncoder(handle_unknown='ignore').fit([['female'], ['male'], ['Other']])
-    enc_title = OneHotEncoder(handle_unknown='ignore').fit([['Mr.'], ['Mrs.'], ['Miss.'], ['Master.'], ['Dr.'],
-                                                            ['Rev.'], ['Other']])
-    enc_cabin = OneHotEncoder(handle_unknown='ignore').fit([['A'], ['B'], ['C'], ['D'], ['E'], ['F'], ['G'], ['Other']])
-    enc_embarked = OneHotEncoder(handle_unknown='ignore').fit([['C'], ['S'], ['Q'], ['Other']])
-    return {'enc_pclass': enc_pclass, 'enc_gender': enc_gender, 'enc_title': enc_title, 'enc_cabin': enc_cabin,
-            'enc_embarked': enc_embarked}
+    test_data = pd.read_csv('test.csv')
+    return dev_data, test_data
 
 
 def clean_name(names):
     title_in_names = []
-    titles = ['Mr.', 'Mrs.', 'Miss.', 'Master.', 'Dr.', 'Rev.']
-    for name in names:
-        contains_title = False
+    titles = ['Mr.', 'Mrs.', 'Miss.', 'Master.']
+    for name in names.values:
+        cleaned_title = 'Other'
         for title in titles:
-            if title in name:
-                title_in_names.append(title)
-                contains_title = True
+            if title in name[0]:
+                cleaned_title = title
                 break
-        if not contains_title:
-            title_in_names.append('Other')
-    return np.reshape(title_in_names, (-1, 1))
+        title_in_names.append(cleaned_title)
+    title_in_names = np.reshape(title_in_names, (-1, 1))
+    return pd.DataFrame(title_in_names, columns=['Name'])
 
 
-def clean_cabin(cabins):
-    cabins_pre = [];
-    cabin_types = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-    for cabin_full in cabins:
-        contains_cabin_type = False
-        for cabin_type in cabin_types:
-            if cabin_type in cabin_full:
-                cabins_pre.append(cabin_type)
-                contains_cabin_type = True
+def clean_ticket(tickets):
+    ticket_abbr_col = []
+    ticket_abbrs = ['1', '2', '3', 'A', 'C', 'P', 'S']
+    for ticket in tickets.values:
+        cleaned_ticket = 'Other'
+        for ticket_abbr in ticket_abbrs:
+            if ticket_abbr in ticket[0]:
+                cleaned_ticket = ticket_abbr
                 break
-        if not contains_cabin_type:
-            cabins_pre.append('Other')
-    return np.reshape(cabins_pre, (-1, 1))
+        ticket_abbr_col.append(cleaned_ticket)
+    ticket_abbr_col = np.reshape(ticket_abbr_col, (-1, 1))
+    return pd.DataFrame(ticket_abbr_col, columns=['Ticket'])
 
 
-def initialize_parameters(n_X):
-    tf.set_random_seed(1)
-    W1 = tf.get_variable('W1', [10, n_X], initializer=tf.contrib.layers.xavier_initializer(seed=1))
-    b1 = tf.get_variable('b1', [10, 1], initializer=tf.zeros_initializer())
-    W2 = tf.get_variable('W2', [10, 10], initializer=tf.contrib.layers.xavier_initializer(seed=1))
-    b2 = tf.get_variable('b2', [10, 1], initializer=tf.zeros_initializer())
-    W3 = tf.get_variable('W3', [1, 10], initializer=tf.contrib.layers.xavier_initializer(seed=1))
-    b3 = tf.get_variable('b3', [1, 1], initializer=tf.zeros_initializer())
-    parameters = {'W1': W1,
-                  'b1': b1,
-                  'W2': W2,
-                  'b2': b2,
-                  'W3': W3,
-                  'b3': b3,
-                  }
-    return parameters
-
-
-def forward_propagation(X, parameters):
-    W1 = parameters['W1']
-    b1 = parameters['b1']
-    W2 = parameters['W2']
-    b2 = parameters['b2']
-    W3 = parameters['W3']
-    b3 = parameters['b3']
-    Z1 = tf.matmul(W1, X) + b1
-    A1 = tf.nn.leaky_relu(Z1)
-    Z2 = tf.matmul(W2, A1) + b2
-    A2 = tf.nn.leaky_relu(Z2)
-    Z3 = tf.matmul(W3, A2) + b3
-    return Z3
-
-
-def compute_cost(parameters, Z3, Y, regularizer_coefficient):
-    logits = tf.transpose(Z3)
-    labels = tf.transpose(Y)
-    sigmoid_cost = tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=labels)
-    #sigmoid_cost = tf.nn.weighted_cross_entropy_with_logits(logits=logits, targets=labels, pos_weight=1)
-    regularizer = tf.nn.l2_loss(parameters['W1'])+tf.nn.l2_loss(parameters['W2'])+tf.nn.l2_loss(parameters['W3'])
-    cost = tf.reduce_mean(sigmoid_cost + regularizer_coefficient * regularizer)
-    return cost
-
-
-def create_placeholders(n_x, n_y):
-    X = tf.placeholder(tf.float32, shape=(n_x, None), name='X')
-    Y = tf.placeholder(tf.float32, shape=(n_y, None), name='Y')
-    return X, Y
-
-
-def model(X_train, Y_train, X_test=[], Y_test=[], learning_rate=0.003, regularization=0,
-          num_epochs=30000):
-    tf.set_random_seed(1)
-    seed = 3
-    (n_x, m) = X_train.shape
-    n_y = Y_train.shape[0]
-    X, Y = create_placeholders(n_x, n_y)
-    parameters = initialize_parameters(n_x)
-    Z3 = forward_propagation(X, parameters)
-    cost = compute_cost(parameters, Z3, Y, regularizer_coefficient=regularization)
-    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
-    init = tf.global_variables_initializer()
-    with tf.Session() as sess:
-        sess.run(init)
-        for epoch in range(num_epochs):
-            seed = seed + 1
-            _, epoch_cost = sess.run([optimizer, cost], feed_dict={X: X_train, Y: Y_train})
-            if seed%1000 == 0:
-                print('Cost: ', epoch_cost)
-        parameters = sess.run(parameters)
-        Z3 = tf.nn.sigmoid(Z3)
-        correct_prediction = tf.equal(tf.round(Z3), Y)
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
-        print('Learning Rate: ', learning_rate)
-        print('Regularization: ', regularization)
-        print('Train Accuracy:', accuracy.eval({X: X_train, Y: Y_train}))
-        #print('Test Accuracy:', accuracy.eval({X: X_test, Y: Y_test}))
-        print()
-        return parameters
-
-
-def predict(X, parameters):
-    W1 = tf.convert_to_tensor(parameters['W1'])
-    b1 = tf.convert_to_tensor(parameters['b1'])
-    W2 = tf.convert_to_tensor(parameters['W2'])
-    b2 = tf.convert_to_tensor(parameters['b2'])
-    W3 = tf.convert_to_tensor(parameters['W3'])
-    b3 = tf.convert_to_tensor(parameters['b3'])
-
-    params = {'W1': W1,
-              'b1': b1,
-              'W2': W2,
-              'b2': b2,
-              'W3': W3,
-              'b3': b3}
-
-    x = tf.placeholder('float', [X.shape[0], X.shape[1]])
-    z3 = forward_propagation(x, params)
-    z3 = tf.nn.sigmoid(z3)
-    p = tf.round(z3)
-    sess = tf.Session()
-    prediction = sess.run(p, feed_dict={x: X})
-    return prediction
-
+def nn_model():
+    model = Sequential()
+    model.add(Dense(20, activation='relu', input_dim=22))
+    model.add(Dense(20, activation='relu'))
+    model.add(Dense(20, activation='relu'))
+    model.add(Dense(20, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='Adam', metrics=["accuracy"])
+    return model
 
 start()
